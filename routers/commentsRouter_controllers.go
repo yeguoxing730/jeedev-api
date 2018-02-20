8b8807d8e368b65911cad8cad0b1956ed81b2594
yeguoxing730 <yeguoxing730@163.comm>
2018-02-20 22:10:38 +0800

    Initial commit

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,50 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["jeedev-api/controllers:AppController"] = append(beego.GlobalControllerRouter["jeedev-api/controllers:AppController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["jeedev-api/controllers:AppController"] = append(beego.GlobalControllerRouter["jeedev-api/controllers:AppController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["jeedev-api/controllers:AppController"] = append(beego.GlobalControllerRouter["jeedev-api/controllers:AppController"],
+		beego.ControllerComments{
+			Method: "GetOne",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["jeedev-api/controllers:AppController"] = append(beego.GlobalControllerRouter["jeedev-api/controllers:AppController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["jeedev-api/controllers:AppController"] = append(beego.GlobalControllerRouter["jeedev-api/controllers:AppController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+}
